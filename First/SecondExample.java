de5e628f39818d88f82bd4c341d832ba9861202a
Md Imran Alam <mdimranalam2348@gmail.com>
2024-09-02 23:58:50 +0530

    Create SecondExample.java

---
+++ First/SecondExample.java
@@ -0,0 +1,9 @@
+public class SecondExample {
+    public static void main(String[] args) {
+        String name[]= {"furkan","imran","Raja","Adil"};
+
+        for (int i=0;i<name.length;i++){
+            System.out.print(name[i]+" ");
+        }
+    }
+}
