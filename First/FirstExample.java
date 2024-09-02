01454e4bcfed1be301674413c0445e86568a9b1f
Md Imran Alam <mdimranalam2348@gmail.com>
2024-09-02 23:51:51 +0530

    Create FirstExample.java

---
+++ First/FirstExample.java
@@ -0,0 +1,13 @@
+public class FirstExample {
+    
+    public static void main(String[] args) {
+
+
+        int array[]={45,25,65,48,35,4};
+
+        for (int i=0;i< array.length;i++){
+
+            System.out.println(array[i]);
+        }
+    }
+}
