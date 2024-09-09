4565a45697e3a374a9e64c836601276d69289313
Md Imran Alam <mdimranalam2348@gmail.com>
2024-09-09 23:55:54 +0530

    Create Second.java

---
+++ Basic/Second.java
@@ -0,0 +1,25 @@
+public class Second {
+
+    public static void main(String[] args) {
+
+        /** Array declaration */
+        String [] str=new String[5];
+
+
+        /** Array initialization */
+
+        str[0]="md imran alam";
+        str[1]="virat";
+        str[2]="md furkan alam";
+        str[3]="shahid ali";
+        str[4]="Rohit";
+
+        /** traverse the Array using the for Each loop*/
+
+      for (String x: str){
+          System.out.println(x);
+      }
+
+    }
+    
+}
