d8f6f0031eec402096a96b06a80e56c26818655a
Md Imran Alam <mdimranalam2348@gmail.com>
2024-09-11 20:37:26 +0530

    Array Simple Program

---
+++ Basic/Simple.java
@@ -0,0 +1,12 @@
+public class Simple {
+    public static void main(String []args){
+
+        int [] arr= new int[] {45,65,25,88,98,45,625,4};
+
+        /** print all Array Element*/
+
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
